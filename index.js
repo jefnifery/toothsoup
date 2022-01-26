f4e4084c819ac211464b8a823b8f63fa6d423232
Jeffery Ni <jefferyni@gmail.com>
2022-01-25 22:00:28 -0800

    SEcond commit

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port);
 
 console.log(`App listening on ${port}`);
