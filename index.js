5be49a5d3538c18e96a8a62a86ffc5b00153ec44
Jeffery Ni <jefferyn@qualtrics.com>
2022-01-26 19:35:08 -0800

    socketio

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 const express = require("express");
 const path = require("path");
-const generatePassword = require("password-generator");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const PORT = process.env.PORT || 8000;
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
 
-// Put all API endpoints under '/api'
-app.get("/api/passwords", (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map((i) =>
-    generatePassword(12, false)
-  );
-
-  // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
-});
-
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-const port = process.env.PORT || 8000;
-app.listen(port);
+//Whenever someone connects this gets executed
+io.on("connection", (socket) => {
+    console.log("A user connected");
 
-console.log(`App listening on ${port}`);
+    //Whenever someone disconnects this piece of code executed
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
