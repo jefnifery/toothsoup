48694db6f74234b666daa5071064b5801536319e
Jeffery Ni <jefferyn@qualtrics.com>
2022-01-29 16:36:43 -0800

    loginpage

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import MainPage from "./components/MainPage";
 import Room from "./components/Room";
+import { Box } from "gestalt";
 
 import "gestalt/dist/gestalt.css";
 import "./App.css";
 
 function App() {
     const [socket, setSocket] = useState(null);
+    const [inRoom, setInRoom] = useState(false);
     const [roomId, setRoomId] = useState("");
     const [username, setUsername] = useState("");
 
@@ -17,18 +19,40 @@ function App() {
         return () => newSocket.close();
     }, [setSocket]);
 
+    const joinRoom = (joinRoomId) => {
+        if (!joinRoomId) {
+            joinRoomId = Math.random().toString(36).slice(2, 8);
+        }
+
+        setRoomId(joinRoomId);
+        setInRoom(true);
+    };
+
     return (
-        <div className="App">
+        <Box paddingX={4} paddingY={4}>
             {socket ? (
-                roomId ? (
-                    <Room socket={socket} roomId={roomId} setRoomId={setRoomId} />
+                inRoom ? (
+                    <Room
+                        socket={socket}
+                        userName={username}
+                        roomId={roomId}
+                        setInRoom={setInRoom}
+                        setRoomId={setRoomId}
+                    />
                 ) : (
-                    <MainPage socket={socket} setRoomId={setRoomId} username={username} setUsername={setUsername} />
+                    <MainPage
+                        socket={socket}
+                        roomId={roomId}
+                        username={username}
+                        joinRoom={joinRoom}
+                        setRoomId={setRoomId}
+                        setUsername={setUsername}
+                    />
                 )
             ) : (
                 <div>Connecting...</div>
             )}
-        </div>
+        </Box>
     );
 }
 
--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { Box, Flex, Button, Text, TextField } from "gestalt";
+import { Flex, Button, Switch, TextField, Text, Heading } from "gestalt";
 
-function MainPage({ socket, setRoomId, username, setUsername }) {
+function MainPage({ socket, roomId, username, joinRoom, setRoomId, setUsername }) {
+    const [joinExistingRoom, setJoinExistingRoom] = useState(false);
+
+    const canJoinRoom = username && ((joinExistingRoom && roomId) || !joinExistingRoom);
+
     return (
-        <Box paddingX={4} paddingY={4}>
-            <Flex alignItems="center" direction="column" gap={4}>
-                <TextField label="enter a username" onChange={({ value }) => setUsername(value)} value={username} />
-                <Button text="join room" onClick={() => setRoomId("tempId")} />
+        <Flex alignItems="center" direction="column" gap={4}>
+            <Heading>toothsoup</Heading>
+            <Flex gap={2}>
+                <Text>create new room</Text>
+                <Switch onChange={({ value }) => setJoinExistingRoom(value)} switched={joinExistingRoom} />
+                <Text>join existing room</Text>
             </Flex>
-        </Box>
+            <TextField label="enter a username" onChange={({ value }) => setUsername(value)} value={username} />
+            {joinExistingRoom && (
+                <TextField label="enter room id" onChange={({ value }) => setRoomId(value)} value={roomId} />
+            )}
+            <Button text="join room" onClick={() => joinRoom(roomId)} disabled={!canJoinRoom} />
+        </Flex>
     );
 }
 
